09fcb6c22f870382cac729b886784069381ce857
dm-guk6 <gzx1205a262b@i.softbank.jp>
2025-05-24 22:42:44 +0900

    a

--- ABC407/C.py
+++ ABC407/C.py
@@ -1,9 +1,8 @@
 S = input()
 S = S[::-1]
+B =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 cnt = int(S[0])
 for i in range(1, len(S)):
-    if int(S[i]) - cnt >= 0:
-        cnt += int(S[i])
-    else:
-        cnt += 10 - abs(int(S[i]) - cnt) % 10
+    cnt += B[int(S[i]) - cnt % 10]
+cnt += len(S)
 print(cnt)
